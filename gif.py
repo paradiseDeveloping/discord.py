0512b182007a05838915da62d26a69e26d378857
Paradise <91400937+paradiseDeveloping@users.noreply.github.com>
2022-02-07 05:02:59 +0100

    Create gif.py
    dont forget to install giphy_client.
    
    hover it and hit install and import.

---
+++ gif.py
@@ -0,0 +1,18 @@
+@bot.command()
+async def gif(ctx, *, q="Error"):
+    api_key = "0Hv8Jh4EbijmQQbTF250BXuu0bKymwvZ"
+    api_instance = giphy_client.DefaultApi()
+
+    try:
+
+        api_responce = api_instance.gifs_search_get(api_key, q, limit=5, rating="pg")
+        lst = list(api_responce.data)
+        giff = random.choice(lst)
+
+        emb = discord.Embed(title="Your Gif")
+        emb.set_image(url=f"https://media.giphy.com/media/{giff.id}/giphy.gif")
+
+        await ctx.channel.send(embed=emb)
+
+    except ApiException as e:
+        print("")
