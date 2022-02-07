f6352899d216bfd9e0c7f787261159ed9d6c23cc
Paradise <91400937+paradiseDeveloping@users.noreply.github.com>
2022-02-07 05:05:11 +0100

    Update gif.py

--- gif.py
+++ gif.py
@@ -1,7 +1,7 @@
 #dont forget to hover giphy_client and hit install + import.
 
 async def gif(ctx, *, q="Error"):
-    api_key = "0Hv8Jh4EbijmQQbTF250BXuu0bKymwvZ"
+    api_key = "insert ur api key here" # https://support.giphy.com/hc/en-us/articles/360020283431-Request-A-GIPHY-API-Key
     api_instance = giphy_client.DefaultApi()
 
     try:
