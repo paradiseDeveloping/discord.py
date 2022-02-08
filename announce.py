fcd4d3826efb08974f5388cfebcacf589163f77d
Paradise <91400937+paradiseDeveloping@users.noreply.github.com>
2022-02-08 17:02:55 +0100

    Update announce.py

--- announce.py
+++ announce.py
@@ -4,7 +4,6 @@ a = "paradise#0666"
 async def announce(ctx,*,arg):
     if ctx.author.guild_permissions.administrator:
         member = ctx.author
-        announcetext = arg
         await ctx.channel.purge(limit=1)
         embed = discord.Embed(title="Announcement",
                               colour=0xDC6BC4,
